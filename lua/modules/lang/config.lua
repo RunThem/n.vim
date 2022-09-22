061bdb90b3fd9a451427e8c946695ef2015f475a
RunThem <iccy.fun@outlook.com>
2022-09-22 20:17:17 +0800

    (config) treesitter, C, Zig & Rust

--- lua/modules/lang/config.lua
+++ lua/modules/lang/config.lua
@@ -4,7 +4,7 @@ function config.nvim_treesitter()
   vim.api.nvim_command('set foldmethod=expr')
   vim.api.nvim_command('set foldexpr=nvim_treesitter#foldexpr()')
   require('nvim-treesitter.configs').setup({
-    ensure_installed = { 'c' },
+    ensure_installed = { 'c', 'zig', 'rust' },
     ignore_install = { 'phpdoc' },
     highlight = {
       enable = true,
