f13c91accf7d5ba3ad5ae5aad0d15e1e4d959d84
RunThem <iccy.fun@outlook.com>
2022-09-24 05:55:42 +0800

    (config) mason & formatter

--- lua/modules/tools/config.lua
+++ lua/modules/tools/config.lua
@@ -99,18 +99,27 @@ function config.autopairs()
       return opts.prev_char:match('.%}') ~= nil
     end):use_key('}')
   })
+
+  local status, cmp = pcall(require, 'cmp')
+  if not status then
+    vim.cmd([[packadd nvim-cmp]])
+  end
+
+  cmp = require('cmp')
+  local pairs = require('nvim-autopairs.completion.cmp')
+  cmp.event:on('confirm_done', pairs.on_confirm_done({ map_char = { tex = '' } }))
 end
 
 function config.pretty_fold()
   require('pretty-fold').setup({
     sections = {
-       left = {
-          'content',
-       },
-       right = {
-          ' ', 'number_of_folded_lines', ' ',
-          function(config) return config.fill_char:rep(3) end
-       }
+      left = {
+        'content',
+      },
+      right = {
+        ' ', 'number_of_folded_lines', ' ',
+        function(config) return config.fill_char:rep(3) end
+      }
     },
     fill_char = '•',
     remove_fold_markers = true,
@@ -118,16 +127,78 @@ function config.pretty_fold()
     process_comment_signs = 'spaces',
     comment_signs = {},
     stop_words = {
-       '@brief%s*', -- (for C++) Remove '@brief' and all spaces after.
+      '@brief%s*' -- (for C++) Remove '@brief' and all spaces after.
     },
     add_close_pattern = true, -- true, 'last_line' or false
     matchup_patterns = {
-       {  '{', '}' },
-       { '%(', ')' }, -- % to escape lua pattern char
-       { '%[', ']' }, -- % to escape lua pattern char
+      {  '{', '}' },
+      { '%(', ')' }, -- % to escape lua pattern char
+      { '%[', ']' }, -- % to escape lua pattern char
     },
-    ft_ignore = { 'neorg' },
+    ft_ignore = { 'neorg' }
   })
+end
+
+function config.mason()
+  require('mason').setup({
+    ui = {
+      check_outdated_packages_on_open = true,
+      border = "none",
+      icons = {
+        package_installed = "◍",
+        package_pending = "◍",
+        package_uninstalled = "◍"
+      },
+      keymaps = {
+        -- Keymap to expand a package
+        toggle_package_expand = "<CR>",
+        -- Keymap to install the package under the current cursor position
+        install_package = "i",
+        -- Keymap to reinstall/update the package under the current cursor position
+        update_package = "u",
+        -- Keymap to check for new version for the package under the current cursor position
+        check_package_version = "c",
+        -- Keymap to update all installed packages
+        update_all_packages = "U",
+        -- Keymap to check which installed packages are outdated
+        check_outdated_packages = "C",
+        -- Keymap to uninstall a package
+        uninstall_package = "X",
+        -- Keymap to cancel a package installation
+        cancel_installation = "<C-c>",
+        -- Keymap to apply language filter
+        apply_language_filter = "<C-f>"
+      }
+    },
+    pip = { install_args = {} },
+    log_level = vim.log.levels.INFO,
+    max_concurrent_installers = 4,
+    github = {
+      download_url_template = "https://github.com/%s/releases/download/%s/%s"
+    }
+  })
+end
+
+function config.formatter()
+  local conf = function(exe, args, stdin)
+    return { exe = exe, args = args, stdin = stdin }
+  end
+
+  require('formatter').setup {
+    filetype = {
+      -- c = {
+      --   function()
+      --     return { exe = 'clang-format', args = { '-i' }, stdin = false }
+      --   end
+      -- },
+      c = conf('clang-format', { '-i' }, false),
+      cpp = conf('clang-format', { '-i' }, false),
+      sh = conf('shfmt', { '-i' }, false),
+      go = conf('gofmt', { '-w' }, false),
+      rust = conf('rustfmt', {}, true),
+      lua = conf('lua-format', { '-i', '-c', '~/.config/nvim/style.toml' }, true)
+    }
+  }
 end
 
 return config
