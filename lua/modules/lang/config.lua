a1dd6caa3904bf8a460d3ecd076d8a8550c3b91c
RunThem <iccy.fun@outlook.com>
2022-09-24 01:22:04 +0800

    (config) treesitter textobjects plugin

--- lua/modules/lang/config.lua
+++ lua/modules/lang/config.lua
@@ -3,20 +3,24 @@ local config = {}
 function config.nvim_treesitter()
   vim.api.nvim_command('set foldmethod=expr')
   vim.api.nvim_command('set foldexpr=nvim_treesitter#foldexpr()')
+
+  for _, conf in pairs(require('nvim-treesitter.parsers').get_parser_configs()) do
+    conf.install_info.url = conf.install_info.url:gsub('https://github.com', 'https://hub.fastgit.org')
+  end
+
   require('nvim-treesitter.configs').setup({
-    ensure_installed = { 'c', 'zig', 'rust' },
-    ignore_install = { 'phpdoc' },
-    highlight = {
-      enable = true,
-    },
+    ensure_installed = { 'c', 'cpp', 'go', 'lua', 'rust' },
+    auto_install = true,
+    highlight = { enable = true },
     textobjects = {
       select = {
         enable = true,
         keymaps = {
-          ['af'] = '@function.outer',
-          ['if'] = '@function.inner',
-          ['ac'] = '@class.outer',
-          ['ic'] = '@class.inner',
+          ['if'] = '@function.outer',    -- function() {...}
+          ['ic'] = '@conditional.inner', -- if() {...}
+          ['il'] = '@loop.inner',        -- for() {...}
+          ['ip'] = '@parameter.inner',   -- , var,
+          ['im'] = '@comment.outer',     -- /*  */
         },
       },
     },
