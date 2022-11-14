32b0f53a02c42ed00a9d072c9c3157b58d00f47d
RunThem <iccy.fun@outlook.com>
2022-11-14 23:42:09 +0800

    (config) ui

--- lua/modules/ui/config.lua
+++ lua/modules/ui/config.lua
@@ -7,6 +7,11 @@ local config = {}
 function config.zephyr()
   vim.cmd('colorscheme zephyr')
   vim.cmd('highlight Normal guibg=NONE guifg=NONE')
+  vim.cmd('highlight LineNr guibg=NONE guifg=#61afaf gui=italic')
+  vim.cmd('highlight CursorLineNr guibg=NONE guifg=#e95678')
+  vim.cmd('highlight SignColumn guibg=NONE')
+  vim.cmd('highlight VertSplit guibg=NONE guifg=#c98afa')
+  vim.cmd('highlight Comment gui=italic')
 end
 
 function config.galaxyline()
