ef4246371a5e1e6e56c82608305bf48495c6f9bb
RunThem <iccy.fun@outlook.com>
2022-11-15 00:26:04 +0800

    (config) lastplace

---
+++ lua/internal/lastplace.lua
@@ -0,0 +1,49 @@
+local fn = vim.fn
+local api = vim.api
+
+local lp = {
+  ['ignore_bt'] = { 'quickfix', 'nofile', 'help' },
+  ['ignore_ft'] = { 'gitcommit', 'gitrebase', 'svn', 'hgcommit' },
+  ['open_folds'] = 1,
+}
+
+local set_cursor_position = function()
+  local last_line = fn.line([['"]])
+  local buff_last_line = fn.line('$')
+  local window_last_line = fn.line('w$')
+  local window_first_line = fn.line('w0')
+
+  if last_line > 0 and last_line <= buff_last_line then
+    if window_last_line == buff_last_line then
+      api.nvim_command([[normal! g`"]])
+    elseif buff_last_line - last_line > ((window_last_line - window_first_line) / 2) - 1 then
+      api.nvim_command([[normal! g`"zz]])
+    else
+      api.nvim_command([[normal! G'"<c-e>]])
+    end
+  end
+
+  if fn.foldclosed('.') ~= -1 and lp.lastplace_open_folds == 1 then
+    api.nvim_command([[normal! zvzz]])
+  end
+end
+
+local lastplace_buf = function()
+  local get_opt = function(type)
+    return api.nvim_buf_get_option(0, type)
+  end
+
+  if vim.tbl_contains(lp.ignore_bt, get_opt('buftype')) or vim.tbl_contains(lp.ignore_ft, get_opt('filetype')) then
+    return
+  end
+
+  if fn.line('.') > 1 then
+    return
+  end
+
+  set_cursor_position()
+end
+
+api.nvim_create_augroup('lastplace', { clear = true })
+api.nvim_create_autocmd('BufWinEnter', { callback = lastplace_buf, group = 'lastplace' })
+api.nvim_create_autocmd('FileType', { callback = lastplace_buf, group = 'lastplace' })
