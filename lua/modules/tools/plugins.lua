49a18e6c8eff9eb60a666a8f0677d6e3bf9e64c5
RunThem <iccy.fun@outlook.com>
2023-01-08 05:00:55 +0800

    (plugin) delaytrain

--- lua/modules/tools/plugins.lua
+++ lua/modules/tools/plugins.lua
@@ -4,10 +4,16 @@
 
 local package = require('core.pack').package
 local setup = require('core.pack').setup
+local conf = require('modules.tools.config')
 
 package({
   'williamboman/mason.nvim',
   config = setup('mason'),
+})
+
+package({
+  'ja-ford/delaytrain.nvim',
+  config = conf.delaytrain,
 })
 
 package({ 'h-hg/fcitx.nvim' })
