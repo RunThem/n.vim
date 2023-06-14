6d3ab10082236bda2698efcb5ef8060acfb75108
RunThem <iccy.fun@outlook.com>
2023-06-14 17:17:06 +0800

    (plugin) vim.ui.input()

--- lua/dev.lua
+++ lua/dev.lua
@@ -4,66 +4,80 @@ local info = require('core.util').info
 local api = vim.api
 local ts = vim.treesitter
 
-local function get_func_info()
-  local syntax_tree = ts.get_parser(0, 'c'):parse()[1]
-  local root = syntax_tree:root()
+function M.window_center(input_width)
+  return {
+    relative = 'win',
+    row = vim.api.nvim_win_get_height(0) / 2 - 1,
+    col = vim.api.nvim_win_get_width(0) / 2 - input_width / 2,
+  }
+end
 
-  local query = ts.parse_query('c', '(function_definition) @func')
-  local func_list = {}
+function M.under_cursor(_)
+  return {
+    relative = 'cursor',
+    row = 1,
+    col = 0,
+  }
+end
 
-  for _, captures, metadata in query:iter_matches(root, 0) do
-    local start_row, start_col = captures[1]:start()
+function M.input(opts, on_confirm, win_config)
+  local prompt = opts.prompt or 'Input: '
+  local default = opts.default or ''
 
-    local end_row = 0
-    local end_col = 0
-    local is_puc = true
-    for i = 0, captures[1]:named_child_count() - 1 do
-      local child = captures[1]:named_child(i)
+  -- Calculate a minimal width with a bit buffer
+  local default_width = vim.str_utfindex(default) + 10
+  local prompt_width = vim.str_utfindex(prompt) + 10
+  local input_width = default_width > prompt_width and default_width or prompt_width
 
-      if child:type() == 'storage_class_specifier' then
-        if ts.query.get_node_text(child, 0) == 'static' then
-          is_puc = false
-        end
-      end
+  local default_win_config = {
+    focusable = true,
+    style = 'minimal',
+    border = 'rounded',
+    width = input_width,
+    height = 1,
+    title = prompt,
+  }
 
-      if child:type() == 'function_declarator' then
-        end_row, end_col = child:end_()
-        break
-      end
-    end
-
-    if end_row == end_col == 0 then
-      goto continue
-    end
-
-    local func = api.nvim_buf_get_text(0, start_row, start_col, end_row, end_col, {nil})
-
-    table.insert(func, is_puc)
-
-    table.insert(func_list, func)
-
-    ::continue::
+  -- Place the window near cursor or at the center of the window.
+  if prompt == 'New Name: ' then
+    default_win_config = vim.tbl_deep_extend('force', default_win_config, M.under_cursor(input_width))
+  else
+    default_win_config = vim.tbl_deep_extend('force', default_win_config, M.window_center(input_width))
   end
 
-  return func_list
+  -- Apply user's window config.
+  win_config = vim.tbl_deep_extend('force', default_win_config, win_config)
+
+  -- Create floating window.
+  local buffer = vim.api.nvim_create_buf(false, true)
+  local window = vim.api.nvim_open_win(buffer, true, win_config)
+  vim.api.nvim_buf_set_text(buffer, 0, 0, 0, 0, { default })
+
+  -- Put cursor at the end of the default value
+  vim.cmd('startinsert')
+  vim.api.nvim_win_set_cursor(window, { 1, vim.str_utfindex(default) + 1 })
+
+  -- Enter to confirm
+  vim.keymap.set({ 'n', 'i', 'v' }, '<cr>', function()
+    local lines = vim.api.nvim_buf_get_lines(buffer, 0, 1, false)
+    if lines[1] ~= default and on_confirm then
+      on_confirm(lines[1])
+    end
+    vim.api.nvim_win_close(window, true)
+    vim.cmd('stopinsert')
+  end, { buffer = buffer })
+
+  -- Esc or q to close
+  vim.keymap.set('n', '<esc>', function()
+    vim.api.nvim_win_close(window, true)
+  end, { buffer = buffer })
+  vim.keymap.set('n', 'q', function()
+    vim.api.nvim_win_close(window, true)
+  end, { buffer = buffer })
 end
 
 function M.setup()
-  local func_list = get_func_info()
-
-  if vim.tbl_isempty(func_list) then
-    return
-  end
-
-  local funcs = {}
-
-  for _, fn in ipairs(func_list) do
-    table.insert(funcs, fn[1])
-  end
-
-  vim.ui.select(funcs, {}, function (_, idx)
-    info(idx)
-  end)
+  M.input({}, nil, {})
 end
 
 return M
