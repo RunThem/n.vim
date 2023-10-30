4dff0c81c851e70f306adcbb6efdeb2d1741be78
RunThem <iccy.fun@outlook.com>
2023-10-30 22:43:56 +0800

    (package) v1.0.0

--- init.lua
+++ init.lua
@@ -6,7 +6,7 @@
   ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║
   ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝
 Neovim init file
-Version: 0.15.1 - 2023/10/22
+Version: 1.0.0 - 2023/10/30
 Maintainer: RunThem
 Website: https://github.com/RunThem/n.vim.git
 --]]
