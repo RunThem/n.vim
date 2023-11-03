f60cd8f79526c26baa26836a26a2eb16ae7b37ea
RunThem <iccy.fun@outlook.com>
2023-11-03 10:31:38 +0800

    (keymap) set complete keymap

--- lua/modules/keymaps.lua
+++ lua/modules/keymaps.lua
@@ -41,3 +41,66 @@ map.n('<leader>fa', function()
   local fzy = require('fzy')
   fzy.execute('rg --no-heading --trim -nH .', fzy.sinks.edit_live_grep)
 end)
+
+-- complete
+map.i('<C-f>', '<C-x><C-f>')
+
+_G.autocmd({ 'CompleteDone' }, {
+  pattern = '*',
+  callback = function()
+    local u = require('core.util')
+
+    local col = u.col()
+    if u.cline():sub(col, col) == '/' then
+      local key = vim.api.nvim_replace_termcodes('<C-x><C-f>', true, false, true)
+      vim.api.nvim_feedkeys(key, 'i', true)
+    end
+  end,
+})
+
+map.i('<C-e>', function()
+  return vim.fn.pumvisible() == 1 and '<C-e>' or '<End>'
+end, { expr = true })
+
+map.i('<Tab>', function()
+  if vim.fn.pumvisible() == 1 then
+    local selected = vim.fn.complete_info({ 'selected' }).selected
+    if selected ~= -1 then
+      return '<C-y>'
+    end
+
+    return '<C-n><C-y>'
+  elseif vim.snippet.jumpable(1) then
+    return cmd('lua vim.snippet.jump(1)')
+  end
+
+  return '<Tab>'
+end, { expr = true })
+
+map.i('<S-Tab>', function()
+  if vim.snippet.jumpable(-1) then
+    return cmd('lua vim.snippet.jump(-1)')
+  end
+
+  return '<S-Tab>'
+end, { expr = true })
+
+map.i('<Cr>', function()
+  if vim.fn.pumvisible() == 1 then
+    local selected = vim.fn.complete_info({ 'selected' }).selected
+    if selected ~= -1 then
+      return '<C-y>'
+    end
+
+    return '<C-n><C-y>'
+  end
+
+  local col = util.col()
+  local s, e = util.cline():find('{%s*}')
+
+  if s ~= nil and col >= s and col <= e then
+    return '<Cr><Esc><Up>o'
+  end
+
+  return '<Cr>'
+end, { expr = true })
