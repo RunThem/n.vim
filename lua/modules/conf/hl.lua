7dc3b2cd24d86c427cbe46d7257bda1cdccc46de
RunThem <iccy.fun@outlook.com>
2024-02-22 20:31:57 +0800

    (config) change ui

--- lua/modules/conf/hl.lua
+++ lua/modules/conf/hl.lua
@@ -1,5 +1,7 @@
 return function()
-  local hl = vim.api.nvim_set_hl
+  local hl = function(name, var)
+    vim.api.nvim_set_hl(0, name, var)
+  end
 
   local groups = {
     'Normal',
@@ -18,7 +20,7 @@ return function()
   }
 
   for _, group in ipairs(groups) do
-    hl(0, group, {})
+    hl(group, {})
   end
 
   local links = {
@@ -39,49 +41,49 @@ return function()
   }
 
   for newgroup, oldgroup in pairs(links) do
-    hl(0, newgroup, { link = oldgroup, default = true })
+    hl(newgroup, { link = oldgroup, default = true })
   end
 
   -- stylua: ignore start
-  hl(0, 'LineNr',                   { fg = '#61afaf' })
-  hl(0, 'CursorWord',               { bg = 'NONE', underline = true })
-  hl(0, 'CursorLine',               { bg = 'NONE', underline = true })
-  hl(0, 'CursorLineNr',             { fg = '#f8ab17', italic = true })
-  hl(0, 'VertSplit',                { fg = '#c98afa' })
-  hl(0, 'Comment',                  { fg = '#6a8ad9' })
-  hl(0, 'PmenuSel',                 { fg = '#000000', bg = '#65CDD9' })
-  hl(0, 'Pmenu',                    { fg = '#C5CDD9', bg = '#000000' })
+  hl('LineNr',                   { fg = '#61afaf' })
+  hl('CursorWord',               { bg = 'NONE', underline = true })
+  hl('CursorLine',               { bg = 'NONE', underline = true })
+  hl('CursorLineNr',             { fg = '#f8ab17', italic = true })
+  hl('VertSplit',                { fg = '#c98afa' })
+  hl('Comment',                  { fg = '#6a8ad9' })
+  hl('PmenuSel',                 { fg = '#000000', bg = '#65CDD9' })
+  hl('Pmenu',                    { fg = '#C5CDD9', bg = '#000000' })
 
-  hl(0, 'AdCustomFold',             { fg = '#bf8040' })
+  hl('AdCustomFold',             { fg = '#bf8040' })
 
-  hl(0, 'CmpItemAbbrDeprecated',    { fg = '#7E8294', bg = 'NONE', strikethrough = true })
-  hl(0, 'CmpItemAbbrMatch',         { fg = '#82AAFF', bg = 'NONE', bold = true })
-  hl(0, 'CmpItemAbbrMatchFuzzy',    { fg = '#82AAFF', bg = 'NONE', bold = true })
-  hl(0, 'CmpItemMenu',              { fg = '#0792EA', bg = 'NONE', italic = true })
-  hl(0, 'CmpItemKindField',         { fg = '#EED8DA', bg = '#B5585F' })
-  hl(0, 'CmpItemKindProperty',      { fg = '#EED8DA', bg = '#B5585F' })
-  hl(0, 'CmpItemKindEvent',         { fg = '#EED8DA', bg = '#B5585F' })
-  hl(0, 'CmpItemKindText',          { fg = '#C3E88D', bg = '#9FBD73' })
-  hl(0, 'CmpItemKindEnum',          { fg = '#C3E88D', bg = '#9FBD73' })
-  hl(0, 'CmpItemKindKeyword',       { fg = '#C3E88D', bg = '#9FBD73' })
-  hl(0, 'CmpItemKindConstant',      { fg = '#FFE082', bg = '#D4BB6C' })
-  hl(0, 'CmpItemKindConstructor',   { fg = '#FFE082', bg = '#D4BB6C' })
-  hl(0, 'CmpItemKindReference',     { fg = '#FFE082', bg = '#D4BB6C' })
-  hl(0, 'CmpItemKindFunction',      { fg = '#EADFF0', bg = '#A377BF' })
-  hl(0, 'CmpItemKindStruct',        { fg = '#EADFF0', bg = '#A377BF' })
-  hl(0, 'CmpItemKindClass',         { fg = '#EADFF0', bg = '#A377BF' })
-  hl(0, 'CmpItemKindModule',        { fg = '#EADFF0', bg = '#A377BF' })
-  hl(0, 'CmpItemKindOperator',      { fg = '#EADFF0', bg = '#A377BF' })
-  hl(0, 'CmpItemKindVariable',      { fg = '#C5CDD9', bg = '#7E8294' })
-  hl(0, 'CmpItemKindFile',          { fg = '#C5CDD9', bg = '#7E8294' })
-  hl(0, 'CmpItemKindUnit',          { fg = '#F5EBD9', bg = '#D4A959' })
-  hl(0, 'CmpItemKindSnippet',       { fg = '#F5EBD9', bg = '#D4A959' })
-  hl(0, 'CmpItemKindFolder',        { fg = '#F5EBD9', bg = '#D4A959' })
-  hl(0, 'CmpItemKindMethod',        { fg = '#DDE5F5', bg = '#6C8ED4' })
-  hl(0, 'CmpItemKindValue',         { fg = '#DDE5F5', bg = '#6C8ED4' })
-  hl(0, 'CmpItemKindEnumMember',    { fg = '#DDE5F5', bg = '#6C8ED4' })
-  hl(0, 'CmpItemKindInterface',     { fg = '#D8EEEB', bg = '#58B5A8' })
-  hl(0, 'CmpItemKindColor',         { fg = '#D8EEEB', bg = '#58B5A8' })
-  hl(0, 'CmpItemKindTypeParameter', { fg = '#D8EEEB', bg = '#58B5A8' })
+  hl('CmpItemAbbrDeprecated',    { fg = '#7E8294', bg = 'NONE', strikethrough = true })
+  hl('CmpItemAbbrMatch',         { fg = '#82AAFF', bg = 'NONE', bold = true })
+  hl('CmpItemAbbrMatchFuzzy',    { fg = '#82AAFF', bg = 'NONE', bold = true })
+  hl('CmpItemMenu',              { fg = '#0792EA', bg = 'NONE', italic = true })
+  hl('CmpItemKindField',         { fg = '#EED8DA', bg = '#B5585F' })
+  hl('CmpItemKindProperty',      { fg = '#EED8DA', bg = '#B5585F' })
+  hl('CmpItemKindEvent',         { fg = '#EED8DA', bg = '#B5585F' })
+  hl('CmpItemKindText',          { fg = '#C3E88D', bg = '#9FBD73' })
+  hl('CmpItemKindEnum',          { fg = '#C3E88D', bg = '#9FBD73' })
+  hl('CmpItemKindKeyword',       { fg = '#C3E88D', bg = '#9FBD73' })
+  hl('CmpItemKindConstant',      { fg = '#FFE082', bg = '#D4BB6C' })
+  hl('CmpItemKindConstructor',   { fg = '#FFE082', bg = '#D4BB6C' })
+  hl('CmpItemKindReference',     { fg = '#FFE082', bg = '#D4BB6C' })
+  hl('CmpItemKindFunction',      { fg = '#EADFF0', bg = '#A377BF' })
+  hl('CmpItemKindStruct',        { fg = '#EADFF0', bg = '#A377BF' })
+  hl('CmpItemKindClass',         { fg = '#EADFF0', bg = '#A377BF' })
+  hl('CmpItemKindModule',        { fg = '#EADFF0', bg = '#A377BF' })
+  hl('CmpItemKindOperator',      { fg = '#EADFF0', bg = '#A377BF' })
+  hl('CmpItemKindVariable',      { fg = '#C5CDD9', bg = '#7E8294' })
+  hl('CmpItemKindFile',          { fg = '#C5CDD9', bg = '#7E8294' })
+  hl('CmpItemKindUnit',          { fg = '#F5EBD9', bg = '#D4A959' })
+  hl('CmpItemKindSnippet',       { fg = '#F5EBD9', bg = '#D4A959' })
+  hl('CmpItemKindFolder',        { fg = '#F5EBD9', bg = '#D4A959' })
+  hl('CmpItemKindMethod',        { fg = '#DDE5F5', bg = '#6C8ED4' })
+  hl('CmpItemKindValue',         { fg = '#DDE5F5', bg = '#6C8ED4' })
+  hl('CmpItemKindEnumMember',    { fg = '#DDE5F5', bg = '#6C8ED4' })
+  hl('CmpItemKindInterface',     { fg = '#D8EEEB', bg = '#58B5A8' })
+  hl('CmpItemKindColor',         { fg = '#D8EEEB', bg = '#58B5A8' })
+  hl('CmpItemKindTypeParameter', { fg = '#D8EEEB', bg = '#58B5A8' })
   -- stylua: ignore end
 end
