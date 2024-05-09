37364bd0d725d229c7faa43b6a40ac39b6e6ec46
RunThem <iccy.fun@outlook.com>
2024-05-09 23:28:53 +0800

    (plugin) update sessions plugin

--- plugin/sessions.lua
+++ plugin/sessions.lua
@@ -1,31 +1,31 @@
-local function session_filepath()
+local sessions_path = util.data_path() .. '/sessions'
+
+local function session_file()
   local path = vim.fn.getcwd(0)
-  return string.format(util.data_path() .. '/sessions/%s.session', vim.base64.encode(path))
+  return string.format('%s/%s.session', sessions_path, vim.base64.encode(path))
 end
 
-local function load_sessions()
-  local file = session_filepath()
-
-  if vim.uv.fs_stat(file) then
-    vim.cmd(string.format('silent! source %s', file))
-  end
-end
-
-local function save_sessions()
-  local file = session_filepath()
-  vim.cmd(string.format('mksession! %s', file))
+if not vim.uv.fs_stat(sessions_path) then
+  vim.uv.fs_mkdir(sessions_path, 493)
 end
 
 autocmd('VimLeavePre', {
   callback = function()
-    save_sessions()
+    local file = session_file()
+    vim.cmd(string.format('mksession! %s', file))
   end,
 })
 
 autocmd('VimEnter', {
   callback = function(opt)
     if opt.file == '' then
-      vim.defer_fn(load_sessions, 20)
+      vim.defer_fn(function()
+        local file = session_file()
+
+        if vim.uv.fs_stat(file) then
+          vim.cmd(string.format('silent! source %s', file))
+        end
+      end, 20)
     end
   end,
 })
