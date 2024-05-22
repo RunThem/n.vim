0a8540f4e6eb20734debd573375808762045f251
RunThem <iccy.fun@outlook.com>
2024-05-22 21:00:05 +0800

    (config) incremental selection

--- lua/mod/conf/ts.lua
+++ lua/mod/conf/ts.lua
@@ -12,50 +12,72 @@ return function()
   })
 
   ---@keymap
-  local node = nil
-  map.n('<Cr>', function()
-    local buf = vim.api.nvim_get_current_buf()
-    node = ts_utils.get_node_at_cursor()
-
-    local text = ts.get_node_text(node, buf)
-    local corrd = { ts.get_node_range(node) }
-
-    if text:find('^[%(%[{]') ~= nil and text:find('[%)%]}]$') ~= nil then
-      corrd[2], corrd[4] = corrd[2] + 1, corrd[4] - 1
+  local curr_node = nil
+  local prev_node = nil
+  local function selection(node, bufnr)
+    if node == nil then
+      return
     end
 
-    vim.cmd({ cmd = 'normal', bang = true, args = { 'v' } }, {})
-    vim.api.nvim_win_set_cursor(0, { corrd[1] + 1, corrd[2] })
+    local coord
+    if
+      vim.bo.filetype == 'c'
+      and prev_node ~= nil
+      and prev_node:type() ~= 'compound_statement'
+      and node:type() == 'for_statement'
+    then
+      local start = { ts.get_node_range(node:child(1)) }
+      local _end = { ts.get_node_range(node:child(5)) }
+
+      coord = { start[1] + 1, start[2] + 1, _end[3] + 1, _end[4] - 1 }
+
+      curr_node = prev_node
+      prev_node = nil
+    else
+      coord = { ts.get_node_range(node) }
+
+      local text = ts.get_node_text(node, bufnr)
+      if text:find('^[%(%[{]') ~= nil and text:find('[%)%]}]$') ~= nil then
+        coord = { coord[1] + 1, coord[2] + 1, coord[3] + 1, coord[4] - 2 }
+      else
+        coord = { coord[1] + 1, coord[2], coord[3] + 1, coord[4] - 1 }
+      end
+    end
+
+    if coord[4] == -1 then
+      return
+    end
+
+    vim.api.nvim_win_set_cursor(0, { coord[1], coord[2] })
     vim.cmd('normal! o')
-    vim.api.nvim_win_set_cursor(0, { corrd[3] + 1, corrd[4] - 1 })
+    vim.api.nvim_win_set_cursor(0, { coord[3], coord[4] })
+  end
+
+  map.n('<Cr>', function()
+    prev_node = nil
+    curr_node = ts_utils.get_node_at_cursor()
+    local bufnr = vim.api.nvim_get_current_buf()
+
+    vim.cmd({ cmd = 'normal', bang = true, args = { 'v' } }, {})
+    selection(curr_node, bufnr)
   end)
 
   map.v('<Cr>', function()
-    if node == nil then
+    if curr_node == nil or curr_node:parent() == nil then
       return
     end
 
-    local buf = vim.api.nvim_get_current_buf()
-    local text = { ts.get_node_text(node, buf), ts.get_node_text(node:parent(), buf) }
+    local parent = curr_node:parent()
+    local bufnr = vim.api.nvim_get_current_buf()
+    local text = { ts.get_node_text(curr_node, bufnr), ts.get_node_text(parent, bufnr) }
 
-    if text[1] == text[2] or text[2]:sub(1, 1) .. text[1] .. text[2]:sub(text[2]:len()) == text[2] then
-      node = node:parent()
+    text[3] = text[2]:sub(1, 1) .. text[1] .. text[2]:sub(text[2]:len())
+    if text[1] == text[2] or text[3] == text[2] then
+      parent = parent:parent()
     end
 
-    node = node:parent()
-    if node == nil then
-      return
-    end
-
-    text = ts.get_node_text(node, buf)
-    local corrd = { ts.get_node_range(node) }
-
-    if text:find('^[%(%[{]') ~= nil and text:find('[%)%]}]$') ~= nil then
-      corrd[2], corrd[4] = corrd[2] + 1, corrd[4] - 1
-    end
-
-    vim.api.nvim_win_set_cursor(0, { corrd[1] + 1, corrd[2] })
-    vim.cmd('normal! o')
-    vim.api.nvim_win_set_cursor(0, { corrd[3] + 1, corrd[4] - 1 })
+    prev_node = curr_node
+    curr_node = parent
+    selection(parent, bufnr)
   end)
 end
