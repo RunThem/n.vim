522c39b0051ff58905290211add84b55f603530c
RunThem <iccy.fun@outlook.com>
2024-05-27 16:44:04 +0800

    (plugin) update session manager

--- plugin/sessions.lua
+++ plugin/sessions.lua
@@ -1,31 +1,25 @@
-local sessions_path = util.datapath('sessions')
+local rootpath = util.datapath('sessions')
+local options = { 'buffers', 'curdir', 'tabpages', 'winsize', 'skiprtp' } -- sessionoptions used for saving
 
 ---init
-if not vim.uv.fs_stat(sessions_path) then
-  vim.uv.fs_mkdir(sessions_path, 493)
+if not vim.uv.fs_stat(rootpath) then
+  vim.uv.fs_mkdir(rootpath, 493)
 end
 
-local function session_file()
-  local path = vim.fn.getcwd(0)
-  return string.format('%s/%s.session', sessions_path, vim.base64.encode(path))
+local function session()
+  local path = string.format('%s/%s.vim', rootpath, vim.fn.getcwd():gsub('/', '%%'))
+  return vim.fn.fnameescape(path)
 end
 
-map.n('<Leader>X', function()
-  local file = session_file()
-  vim.cmd(string.format('mksession! %s', file))
-  vim.notify('Save session!!!')
+map.n('<Leader>x', function()
+  vim.cmd('silent! source ' .. session())
 end)
 
-util.autocmd({ 'VimEnter' }, {
-  callback = function(opt)
-    if opt.file == '' then
-      vim.defer_fn(function()
-        local file = session_file()
-
-        if vim.uv.fs_stat(file) then
-          vim.cmd(string.format('silent! source %s', file))
-        end
-      end, 20)
-    end
+util.autocmd({ 'VimLeavePre' }, {
+  callback = function()
+    local tmp = vim.o.sessionoptions
+    vim.o.sessionoptions = table.concat(options, ',')
+    vim.cmd('mksession! ' .. session())
+    vim.o.sessionoptions = tmp
   end,
 })
