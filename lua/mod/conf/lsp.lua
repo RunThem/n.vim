8818abe744eb6d352d90732128f81c3b908dc5b1
RunThem <iccy.fun@outlook.com>
2024-09-03 01:45:16 +0800

    (config) lua diagnostics = false

--- lua/mod/conf/lsp.lua
+++ lua/mod/conf/lsp.lua
@@ -18,6 +18,10 @@ lsp_conf['v_analyzer'] = {
 lsp_conf['lua_ls'] = {
   settings = {
     Lua = {
+      diagnostics = {
+        enable = false,
+        globals = { 'vim', 'packer_plugins' },
+      },
       runtime = { version = 'Lua 5.4' },
       completion = { keywordSnippet = 'Disable', callSnippet = 'Replace' },
     },
