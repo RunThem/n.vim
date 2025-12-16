03a8877f213974b75f6b5cad732e9806a2f822aa
RunThem <iccy.fun@outlook.com>
2025-12-16 21:01:42 +0800

    (config) add gopls & xmake_ls config

---
+++ lsp/xmake_ls.lua
@@ -0,0 +1,13 @@
+---@brief
+---@type vim.lsp.Config
+-- return {
+--   cmd = { 'xmake_ls' },
+--   filetypes = { 'lua' },
+--   root_markers = {
+--     'xmake.lua',
+--   },
+--   settings = {
+--     Lua = {},
+--   },
+--   workspace_required = false,
+-- }
